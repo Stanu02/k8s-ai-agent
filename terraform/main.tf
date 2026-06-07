9cf17268ac4f8857e7389cb299ff36febd792297
Ubuntu <azureuser@devops-vm.tyhvm3ryhwhuzovbfo52mbggih.cx.internal.cloudapp.net>
2026-06-07 06:49:36 +0000

    Add Terraform config for K8s infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "terraform-demo"
+  }
+}
+ 
